e3cbb045d9a78e23a704fc9211006d1f0502155f
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-02-26 11:59:48 +0900

    Rename

--- src/renderer/page/root.jsx
+++ src/renderer/page/root.jsx
@@ -4,24 +4,19 @@ import { connect } from 'react-redux';
 import StandbyPage from './standby';
 import EditorPage from './editor';
 
-class App extends React.Component {
-  render() {
-    const { route } = this.props;
-
-    if (route === 'standby') {
-      return <StandbyPage />;
-    }
-
-    if (route === 'editor') {
-      return <EditorPage />;
-    }
-
-    return <div>404</div>;
+const Root = ({ route }) => {
+  if (route === 'standby') {
+    return <StandbyPage />;
   }
-}
+  if (route === 'editor') {
+    return <EditorPage />;
+  }
+
+  return <div>404</div>;
+};
 
 const mapStateToProps = state => ({
   route: state.route,
 });
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(Root);
