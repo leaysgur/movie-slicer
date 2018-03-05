05ff89b77a0a72ba19cf79abc9438a1612401e09
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-05 13:00:27 +0900

    Fix icon ev bug

--- src/renderer/component/icon.jsx
+++ src/renderer/component/icon.jsx
@@ -4,7 +4,7 @@ const Icon = ({ name, size = 'm', anim = 'none', onClick }) => {
   const Icon = <img className={`Icon -${size} -${anim}`} src={`./icon/${name}.svg`} alt={name} />;
   if (typeof onClick === 'function') {
     return (
-      <button className={`IconButton -${size}`} onClick={() => onClick()}>
+      <button className={`IconButton -${size}`} onClick={ev => onClick(ev)}>
         {Icon}
       </button>
     );
