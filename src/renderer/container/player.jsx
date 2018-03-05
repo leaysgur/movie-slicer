a17038d6be81016a9501e2032f465e341c3a1724
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-05 19:38:48 +0900

    Keyboard shortcut

--- src/renderer/container/player.jsx
+++ src/renderer/container/player.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { inject } from 'mobx-react';
+import Mousetrap from 'mousetrap';
 
 import Video from '../component/video';
 import PlayerControls from '../component/player-controls';
 
-const PlayerContainer = ({
-  movie,
-  timeline,
-  event,
-}) => (
-  <div className="L-Player">
-    <Video
-      movie={movie}
-      timeline={timeline}
-      onTimeUpdate={el => event.getVideoCurrentTime(el)}
-    />
-    <div className="L-Player_Controls">
-      <PlayerControls
-        movie={movie}
-        onClickTogglePause={() => event.togglePause()}
-        onClickToggleMute={() => event.toggleMute()}
-      />
-    </div>
-  </div>
-);
+class PlayerContainer extends React.Component {
+  render() {
+    const {
+      movie,
+      timeline,
+      event,
+    } = this.props;
+    return (
+      <div className="L-Player">
+        <Video
+          movie={movie}
+          timeline={timeline}
+          onTimeUpdate={el => event.getVideoCurrentTime(el)}
+        />
+        <div className="L-Player_Controls">
+          <PlayerControls
+            movie={movie}
+            onClickTogglePause={() => event.togglePause()}
+            onClickToggleMute={() => event.toggleMute()}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    Mousetrap.bind('space', () => this.props.event.togglePause());
+  }
+  componentWillUnmount() {
+    Mousetrap.unbind('space');
+  }
+}
 
 export default inject('movie', 'timeline', 'event')(PlayerContainer);
